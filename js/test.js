e8765b8b6458c828ac227b5d3a6bc73bfc9128a6
Chloe Ramos <chaspen27alt@gmail.com>
2020-12-09 09:33:13 -0500

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,18 +1,15 @@
 var set = {
-	album : ToPimpButterfly,
-	artist: DorianElectra
+	album : Yeezus,
+	artist: KanyeWest
 }
 
-var malePrn = ["he", "him", "his", "himself"];
+var malePrn = ["he", "his", "him", "himself"];
 var femalePrn = ["she", "her", "hers", "herself"];
-var neuterPrn = ["they", "them", "theirs", "their", "themself"];
+var neuterPrn = ["they", "their", "theirs", "themself", "them"];
 
 //album info
 
 $.getJSON( "https://musicbrainz.org/ws/2/release/" + set.album + "?inc=aliases+artist-credits+labels+discids+recordings&fmt=json", function( album ) {
-	
-	
-
 	$("#album").html( album.title);
 	$("#artist").html( album['artist-credit'][0].artist.name );
 	$("#tracks").html( album.media[0]['track-count'] );
@@ -22,28 +19,18 @@ $.getJSON( "https://musicbrainz.org/ws/2/release/" + set.album + "?inc=aliases+a
 	//cover art
 	$.getJSON( "https://coverartarchive.org/release/" + set.album + "", function( cover ) {
 		$("#cover").attr("src", cover.images[0].thumbnails.small);
-
 		//artist info
 		$.getJSON( "https://musicbrainz.org/ws/2/artist/" + set.artist + "?inc=aliases&fmt=json", function( artist ) {
 			$("#name").html( artist.name);
-			if ( artist.gender == "Male" ) {
-				$("#pronounSentExample").html( "Album by " + artist.name);
-				$("#pronounSentExample2").html( artist.name + " and " + malePrn[2] + " studio album " + album.title );
+			
+			//Refactored code by https://github.com/fantoro
+			var prn
+			if( artist.gender == "Other" || artist.gender == null ) {
+				prn = neuterPrn[1];
+			} else {
+				prn = ((artist.gender == "Male") ? malePrn[1] : femalePrn[1]);
 			}
-			else if ( artist.gender == "Female" ) {
-				$("#pronounSentExample").html( "Album by " + artist.name);
-				$("#pronounSentExample2").html( artist.name + " and " + femalePrn[1] + " studio album " + album.title );
-			}
-			else if ( artist.gender == "Other" ) {
-				$("#pronounSentExample").html( "Album by " + artist.name);
-				$("#pronounSentExample2").html( artist.name + " and " + neuterPrn[3] + " studio album " + album.title );
-			}
-			else if ( artist.gender == null ) {
-				$("#pronounSentExample").html( "Album by " + artist.name);
-				$("#pronounSentExample2").html( artist.name + " and " + neuterPrn[3] + " studio album " + album.title );
-			}
-
-
+			$('#pronounSentExample2').html( artist.name + " and " + prn + " studio album " +  album.title)
 
 			$.getJSON( "https://restcountries.eu/rest/v2/alpha/" + artist.country , function( nation ) {
 				$("#country").html( nation.name);
